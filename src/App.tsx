56d2afaeac5f3c00ba7603ebf38c5fbc08c73c09
Gianluca Nitti <gianluca.nitti96@gmail.com>
2017-06-23 18:07:07 +0200

    Set up simple layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,53 @@
-/*
-* This file demonstrates a basic ReactXP app.
-*/
-
 import RX = require('reactxp');
-
-import MainPanel = require('./MainPanel');
-import SecondPanel = require('./SecondPanel');
-
-enum NavigationRouteId {
-    MainPanel,
-    SecondPanel
-}
+import { VirtualListView, VirtualListViewItemInfo } from 'reactxp-virtuallistview';
 
 const styles = {
-    navCardStyle: RX.Styles.createViewStyle({
-        backgroundColor: '#f5fcff'
-    })
+  flex: RX.Styles.createViewStyle({
+    flex: 1
+  }),
+  box: RX.Styles.createViewStyle({
+    margin: 8,
+    padding: 8,
+    borderRadius: 4
+  }),
+  greyBorder: RX.Styles.createViewStyle({
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#808080'
+  }),
+  headerView: RX.Styles.createViewStyle({
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#259c9c',
+  }),
+  headerText: RX.Styles.createTextStyle({
+    fontSize: 20,
+    color: 'white'
+  }),
+  contentView: RX.Styles.createViewStyle({
+    flexDirection: 'row',
+    flex: 1
+  }),
 };
 
 class App extends RX.Component<{}, null> {
-    private _navigator: RX.Navigator;
 
-    componentDidMount() {
-        this._navigator.immediatelyResetRouteStack([{
-            routeId: NavigationRouteId.MainPanel,
-            sceneConfigType: RX.Types.NavigatorSceneConfigType.Fade
-        }]);
-    }
-
-    render() {
-        return (
-            <RX.Navigator
-                ref={ this._onNavigatorRef }
-                renderScene={ this._renderScene }
-                cardStyle={ styles.navCardStyle }
-            />
-        );
-    }
-
-    private _onNavigatorRef = (navigator: RX.Navigator) => {
-        this._navigator = navigator;
-    }
-
-    private _renderScene = (navigatorRoute: RX.Types.NavigatorRoute) => {
-        switch (navigatorRoute.routeId) {
-            case NavigationRouteId.MainPanel:
-                return <MainPanel onPressNavigate={ this._onPressNavigate } />
-
-            case NavigationRouteId.SecondPanel:
-                return <SecondPanel onNavigateBack={ this._onPressBack } />
-        }
-
-        return null;
-    }
-
-    private _onPressNavigate = () => {
-        this._navigator.push({
-            routeId: NavigationRouteId.SecondPanel,
-            sceneConfigType: RX.Types.NavigatorSceneConfigType.FloatFromRight,
-            customSceneConfig: {
-                hideShadow: true
-            }
-        });
-    }
-
-    private _onPressBack = () => {
-        this._navigator.pop();
-    }
+  render() {
+    return <RX.View style={styles.flex}>
+      <RX.View style={[styles.box, styles.headerView]}>
+        <RX.Text style={styles.headerText}>Terasology Server web interface</RX.Text>
+        <RX.Text>Unauthenticated mode - click to login</RX.Text>
+      </RX.View>
+      <RX.View style={styles.contentView}>
+        <RX.View style={[styles.box, styles.greyBorder]}>
+          <RX.Text>Menu</RX.Text>
+        </RX.View>
+        <RX.View style={[styles.box, styles.greyBorder, styles.flex]}>
+          <RX.Text>Main panel</RX.Text>
+        </RX.View>
+      </RX.View>
+    </RX.View>;
+  }
 }
 
 export = App;
