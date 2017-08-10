dae345e14c8340944f0406abe3df18a7b4143df5
Gianluca Nitti <gianluca.nitti96@gmail.com>
2017-08-10 12:26:00 +0200

    Allow to manually add server admins by ID

--- src/tabs/serverAdmins/ServerAdminsTabController.tsx
+++ src/tabs/serverAdmins/ServerAdminsTabController.tsx
@@ -3,12 +3,20 @@ import {ServerAdminsTabState} from "./ServerAdminsTabState";
 
 export class ServerAdminsTabController extends TabController<ServerAdminsTabState> {
 
+  public addAdmin = (newAdminId: string) => {
+    this.writeAdminsResource(newAdminId, "ADD");
+  }
+
   public removeAdmin = (adminId: string) => {
+    this.writeAdminsResource(adminId, "REMOVE");
+  }
+
+  private writeAdminsResource = (clientId: string, action: string) => {
     this.model.requestResource({
       action: "WRITE",
       data: {
-        action: "REMOVE",
-        clientId: adminId,
+        action,
+        clientId,
       },
       resourceName: "serverAdmins",
     });
